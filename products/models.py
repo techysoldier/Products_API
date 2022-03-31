ab653c1295d49523cf20b531640c47e9443ddcce
Devin-Symone Johnson <devinsymone@gmail.com>
2022-03-31 16:59:38 -0400

    export postman

--- products/models.py
+++ products/models.py
@@ -7,3 +7,4 @@ class Product(models.Model):
     decription = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
+   
