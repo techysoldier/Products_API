89298a6f9e5568cc28cbc7f56988199a7f1e2e8d
Devin-Symone Johnson <devinsymone@gmail.com>
2022-03-31 16:20:23 -0400

    updated view function + getting products by id and urls path for product detail view function

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
+from products import serializers
 
 
 @api_view(['GET', 'POST'])
@@ -19,21 +20,31 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+@api_view(['GET'])
+def product_detail(request, pk):
+    try:
+        product =Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
+
+    # car = get_object_or_404(Product, pk=pk)
+
+    # if request.method == 'GET':
+    #    serializer = ProductSerializer(car);
+    #    return Response(serializer.data)
    
         
 
-# @api_view(['GET', 'PUT', 'DELETE'])
-# def cars_detail(request, pk):
-#     car = get_object_or_404(Car, pk=pk)
 
-#     if request.method == 'GET':
-#        serializer = CarSerializer(car);
-#        return Response(serializer.data)
+
 
 #     elif request.method == 'PUT':
-#        car = get_object_or_404(Car, pk=pk)
-#        serializer = CarSerializer(car, data=request.data )
+#        product = get_object_or_404(Product, pk=pk)
+#        serializer = ProductSerializer(product, data=request.data )
 #        serializer.is_valid(raise_exception=True)
 #        serializer.save()
 #        return Response(serializer.data)
