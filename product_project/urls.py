161beb2409fea00d8e1ff08c9fed38dcaa2165bf
Devin-Symone Johnson <devinsymone@gmail.com>
2022-03-31 12:45:39 -0400

    products urls added, views.py updates, url configured

--- product_project/urls.py
+++ product_project/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from ast import arg
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', include('products.urls'))
 ]
+
--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view(['GET'])
+def products_list(request):
 
-# Create your views here.
+    return Response('ok')
